b932ad9d3052ba49e4cc52644604b24ca4a75667
Chris-Liter <136271203+Chris-Liter@users.noreply.github.com>
2024-04-29 23:44:55 -0500

    f

--- servidor.py
+++ servidor.py
@@ -22,14 +22,11 @@ server.listen()
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
-channel.exchange_declare(exchange='logs', exchange_type='fanout')
-result = channel.queue_declare(queue='chat_queue')
-queue_name = result.method.queue
-channel.queue_bind(exchange='logs', queue=queue_name)
+cha = channel.queue_declare(queue='chat_queue')
+result = cha.method.queue
 
 clients = []
 nicknames = []
-mensajes = []
 
 def broadcast(message):
     print("Este es el mensaje a enviar: ",message)
@@ -45,12 +42,12 @@ def handle(client):
             if not message:
                 break
             print(f"Mensaje recibido: {message}")
-            mensajes.append(message)
             broadcast(message)
         except Exception as e:
             print(f"Error al manejar cliente: {e}")
             break
 
+    # Cuando el cliente se desconecta, eliminarlo de la lista de clientes
     try:
         index = clients.index(client)
         clients.remove(client)
@@ -69,29 +66,16 @@ def receive():
         nickname = f"Cliente-{address[1]}\n"
         nicknames.append(nickname)
         clients.append(client)
-        print(f"Nombre del cliente: {nickname}\n")
-        broadcast(f"{nickname} se ha conectado\n".encode('utf-8'))
 
-        client.send(f"Bienvenido, {nickname}".encode('utf-8'))  
+        print(f"Nombre de el cliente es {nickname}\n")
+        mensajes = channel.basic_consume(queue=result, on_message_callback=receive, auto_ack=True)
+        broadcast(f"{nickname} \n".encode('utf-8'))
+        #client.send(mensajes)
+        client.send(nickname.encode('utf-8'))  
 
-         
 
-        # Consumir mensajes de la cola y enviarlos al cliente recién conectado
-        def consume_and_send():
-            for method_frame, properties, body in channel.consume(queue=queue_name, inactivity_timeout=1):
-                if body is not None:
-                    message = body.decode('utf-8')
-                    print("Mensaje recibido de RabbitMQ: ", message)
-                    client.send(message.encode('utf-8'))
-
-                # Si hay una desconexión del cliente, detener el consumo de la cola
-                if client.fileno() == -1:
-                    channel.cancel()
-                    break
-
-        consume_thread = threading.Thread(target=consume_and_send)
-        consume_thread.start()
-
+        handle_thread = threading.Thread(target=handle, args=(client,))
+        handle_thread.start()
 
 
 
